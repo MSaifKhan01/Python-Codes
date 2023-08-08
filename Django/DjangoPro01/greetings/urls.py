e4aa572c323177bcdb8ce01ab0d366f270fa7450
MSaifKhan01 <msaifkhan5038@gmail.com>
2023-08-08 19:22:16 +0530

    Django_Project01

---
+++ Django/DjangoPro01/greetings/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+
+urlpatterns=[
+    path("welcome/",views.Welcome,name="welcome"),
+    path('hello/<str:name>',views.Hello,name="Hello"),
+    path('farewell/<str:greet>',views.Farewell,name="farewell")
+]
